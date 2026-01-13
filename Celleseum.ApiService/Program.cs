32302737610be18c1ea41eba09bff1a4353af448
IV082302 <ivan.zhurakhovsky_ext@tecan.com>
2026-01-13 10:12:56 +0100

    Changes for the db-connect

--- Celleseum.ApiService/Program.cs
+++ Celleseum.ApiService/Program.cs
@@ -1,22 +1,36 @@
-
 using Celleseum.Data;
+using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString
+
+var host = builder.Configuration["DatabaseHost"] ?? "localhost";
+var port = builder.Configuration["DatabasePort"] ?? "5432";
+var database = builder.Configuration["Database"];
+var dbUser = builder.Configuration["DatabaseUser"];
+var dbPassword = builder.Configuration["DatabasePassword"];
+
+var connectionString = $"Host={host};Port={port};Database={database};Username={dbUser};Password={dbPassword};Pooling=true";
 
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
 
 // Add services to the container.
 builder.Services.AddProblemDetails();
+builder.Services.AddDbContext<CellesseumDbContext>(options =>
+    options.UseNpgsql(connectionString, npgsql => npgsql.EnableRetryOnFailure()));
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Apply EF Core migrations at startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<CellesseumDbContext>();
+    await db.Database.MigrateAsync();
+}
+
 app.UseExceptionHandler();
 
 if (app.Environment.IsDevelopment())
@@ -24,7 +38,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.MapGet("/turn", async (CellesseumDbContext db) =>
+app.MapGet("/turn", async (CellesseumDbContext db, HttpContext httpContext) =>
 {
     Random rnd = new();
     var numbers = new int[100];
@@ -33,6 +47,14 @@ app.MapGet("/turn", async (CellesseumDbContext db) =>
         numbers[i] = rnd.Next(1, 101);
     }
     var numbersSet = new NumberSet(numbers);
+    var dbRecord = new NumberSetDbRecord
+    {
+        DateTime = DateTime.UtcNow,
+        Average = numbersSet.Average,
+        IpAddress = httpContext.Connection.RemoteIpAddress?.ToString()
+    };
+    db.NumberSets.Add(dbRecord);
+    await db.SaveChangesAsync();
     return numbersSet;
 })
 .WithName("NextTurn");
@@ -50,6 +72,5 @@ record NumberSet
     }
 
     public int[] Numbers { get; init; }
-
     public int Average => Numbers.Length > 0 ? Numbers.Sum() / Numbers.Length : 0;
 }
---
+++ Celleseum.Data/Migrations/20260108094355_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Celleseum.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NumberSets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Average = table.Column<int>(type: "integer", nullable: false),
+                    IpAddress = table.Column<string>(type: "character varying(45)", maxLength: 45, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NumberSets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NumberSets");
+        }
+    }
+}
